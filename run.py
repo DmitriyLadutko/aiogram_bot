415162cbaf73b5f1edca29398fd11e6367dc058b
DmitriyLadutko <dmitriy@example.com>
2025-11-15 18:06:23 +0300

    коммент к харнилищу для FSM, не нужно но добавил

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 import asyncio
 import os
 from dotenv import load_dotenv
-
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-
 from database import Database
 from handlers import BotHandlers
 
@@ -17,17 +15,13 @@ URL = os.getenv("URL")
 async def main():
     if not TOKEN:
         raise RuntimeError("TG_API_KEY не задан в .env")
-
     bot = Bot(token=TOKEN)
-    storage = MemoryStorage()
+    storage = MemoryStorage() # На всякий, dp его создает сам
     dp = Dispatcher(storage=storage)
-
     handlers = BotHandlers(url=URL)
     dp.include_router(handlers.router)
-
     await Database.init(DB_PATH)
     await Database.init_db()
-
     try:
         await dp.start_polling(bot)
     finally:
